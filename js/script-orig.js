1391bd6a4b8ee3cb9dfa137893d7487619f69dc1
Keehun Nam <keehun.nam@code42.com>
2013-12-26 22:31:26 -0600

    Fixing broken link

--- js/script-orig.js
+++ js/script-orig.js
@@ -1,4 +1,3 @@
-
 $(document).ready(function(e) {
 	$('.with-hover-text, .regular-link').click(function(e){
 		e.stopPropagation();
@@ -125,31 +124,4 @@ jQuery(document).ready(function ($) {
 		goToByScroll(dataslide);
 		// $(".nav-collapse").collapse('hide');
 	});
-});
-
-/***************
-* = Menu hover *
-***************/
-jQuery(document).ready(function ($) {
-	//Cache some variables
-	var menu_item = $('.nav').find('li');
-	
-	menu_item.hover(
-		function(e) {
-			var icon = $(this).find('.icon');
-			
-			var left_pos = icon.offset().left - $('.nav').offset().left;
-			var el_width = icon.width() + $(this).find('.text').width() + 10;
-			
-			var hover_bar = $('<div class="active-menu special-active-menu"></div>')
-				.css('left', left_pos)
-				.css('width', el_width)
-				.attr('id', 'special-active-menu-' + $(this).data('slide') );
-			
-			$('.active-menu').after( hover_bar );
-		},
-		function(e) {
-			$('.special-active-menu').remove();
-		}
-	);
 });
